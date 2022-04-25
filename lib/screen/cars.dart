dfdcee1075fb86d667524c82049b82da615a06ac
Muhmmad Gimnastiar <adagimnas@gmail.com>
2022-04-25 21:03:37 +0800

    Solve shared_pref

--- lib/screen/cars.dart
+++ lib/screen/cars.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:rental_santuy/style/colors.dart';
+import 'package:rental_santuy/data/cars_data.dart' as cars;
+import 'package:rental_santuy/data/keys.dart';
+import 'package:rental_santuy/main.dart';
+import 'package:rental_santuy/screen/login.dart';
 import 'package:rental_santuy/style/text.dart';
 import 'package:rental_santuy/widget/card.dart';
-import 'package:rental_santuy/controller/login_controller.dart' as login;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Kendaraan extends StatefulWidget {
-  const Kendaraan({Key? key}) : super(key: key);
+  final SharedPreferences sharedPrefs;
+  const Kendaraan(this.sharedPrefs, {Key? key}) : super(key: key);
 
   @override
   State<Kendaraan> createState() => _KendaraanState();
@@ -15,12 +18,10 @@ class Kendaraan extends StatefulWidget {
 class _KendaraanState extends State<Kendaraan> {
   String title = "Cars";
   bool isFavorite = true;
-  String? nama = "";
+  String nama = "";
 
   void getNamaFromLocal() async {
-    final pref = await SharedPreferences.getInstance();
-    nama = pref.getString('nama');
-    print("Getnama from widget cars ${pref.getString('nama')}");
+    nama = widget.sharedPrefs.getString(Keys.nama) ?? "null";
     setState(() {});
   }
 
@@ -30,77 +31,65 @@ class _KendaraanState extends State<Kendaraan> {
     super.initState();
   }
 
+  List<Map<String, dynamic>> carsList = cars.CarsData.Cars;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          elevation: 0,
-          leading: const Icon(
-            Icons.menu,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: const Icon(
+          Icons.menu,
+        ),
+        title: Center(child: TextLarge(title)),
+        actions: [
+          InkWell(
+            onTap: (() async {
+              widget.sharedPrefs.clear();
+              Navigator.popAndPushNamed(context, "/");
+              main();
+            }),
+            child: Image.asset(
+              'lib/assets/avatars/Avatar.png',
+              width: 32,
+            ),
           ),
-          title: Center(child: TextLarge(title)),
-          actions: [
-            InkWell(
-              onTap: (() async {
-                SharedPreferences prefs = await SharedPreferences.getInstance();
-                prefs.clear();
-                Navigator.popAndPushNamed(context, "/");
-              }),
-              child: Image.asset(
-                'lib/assets/avatars/Avatar.png',
-                width: 32,
+          const SizedBox(
+            width: 24,
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const TextMedium("Hello,"),
+                  TextLarge(nama == 'null' ? Login.nama : nama),
+                ],
               ),
             ),
-            const SizedBox(
-              width: 24,
-            )
+            ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: carsList.length,
+                itemBuilder: (context, index) {
+                  return CardRental(
+                    carsList[index]['name'],
+                    carsList[index]['price'],
+                    widget.sharedPrefs,
+                    uid: carsList[index]['uid'],
+                    image: carsList[index]['image'],
+                    color: carsList[index]['color'],
+                  );
+                }),
           ],
         ),
-        body: SingleChildScrollView(
-          physics: const BouncingScrollPhysics(),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(
-                height: 24,
-              ),
-              Padding(
-                padding: EdgeInsets.all(24),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const TextMedium("Hello,"),
-                    TextLarge(login.login.nama.toString()),
-                  ],
-                ),
-              ),
-              const CardRental(
-                "President car",
-                750,
-                image: "lib/assets/kendaraan/pintuTiga.png",
-                color: MyColors.cokelatSld,
-              ),
-              const CardRental(
-                "Sedan",
-                200,
-                color: MyColors.greySld,
-                image: "lib/assets/kendaraan/sedan.png",
-              ),
-              const CardRental(
-                "Taxi",
-                250,
-                image: "lib/assets/kendaraan/taxi.png",
-                color: MyColors.orangeSld,
-              ),
-              const CardRental(
-                "Electric Car",
-                300,
-                image: "lib/assets/kendaraan/electric.png",
-                color: MyColors.greensSld,
-              ),
-            ],
-          ),
-        ));
+      ),
+    );
   }
 }
