1207d4bb28a8b9e097d05063244d3438c4de9e8b
Muhmmad Gimnastiar <adagimnas@gmail.com>
2022-05-31 11:18:35 +0700

    add routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:rental_santuy/data/keys.dart';
+import 'package:rental_santuy/screen/bicycles.dart';
 import 'package:rental_santuy/screen/cars.dart';
 import 'package:rental_santuy/screen/login.dart';
-import 'package:rental_santuy/controller/login_controller.dart' as login;
+import 'package:rental_santuy/screen/mainpage.dart';
+import 'package:rental_santuy/screen/motors.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main() async {
@@ -22,10 +24,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Rental',
-      initialRoute: isLogin == false ? '/' : '/kendaraan',
+      initialRoute: isLogin == false ? '/' : '/main',
       routes: {
         "/": (context) => Login(sharedPrefs),
-        "/kendaraan": (context) => Kendaraan(sharedPrefs),
+        "/car": (context) => Cars(sharedPrefs),
+        "/main": (context) => MainPage(sharedPrefs),
+        "/motors": (context) => Motors(sharedPrefs),
+        "/bicycles": (context) => Bicycles(sharedPrefs),
       },
     );
   }
